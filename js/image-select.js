ae2f8f6e7896f9ac956d3ed7e9d232675f833f5f
Roma Tereshchenko <romabelka43@gmail.com>
2023-07-31 17:06:02 +0300

    added content for product-info page

---
+++ js/image-select.js
@@ -0,0 +1,11 @@
+const scrollImgs = document.querySelectorAll('.scroll-menu img');
+document.getElementById('preview-img').src = scrollImgs[0].src;
+
+scrollImgs.forEach(img => {
+    img.addEventListener('click', selectImg);
+});
+
+function selectImg(e) {
+    const previewImg = document.getElementById('preview-img');
+    previewImg.src = e.target.src;
+}
