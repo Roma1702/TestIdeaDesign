1e9d8c5ab5fa898a7a0801fb712a6ad7b0d2ffa8
Roma Tereshchenko <romabelka43@gmail.com>
2023-08-10 16:30:17 +0300

    some fixes

--- js/image-select.js
+++ js/image-select.js
@@ -9,3 +9,13 @@ function selectImg(e) {
     const previewImg = document.getElementById('preview-img');
     previewImg.src = e.target.src;
 }
+
+$("#preview-wrapper").on('click', function() {
+    const previewImg = document.getElementById('preview-img');
+    $.fancybox.open({
+        src  : previewImg.src,
+        buttons : [
+            "close"
+        ]
+    });
+});
